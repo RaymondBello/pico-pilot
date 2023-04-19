d6495388dea6e2dec57b24fe4ab2c93efe432e18
Ray <raymondbello11@gmail.com>
2023-04-19 17:59:17 -0400

    added pico-sdk and chibiOS w flash support

---
+++ main/main.cpp
@@ -0,0 +1,46 @@
+
+#include <stdio.h>
+#include "pico/stdlib.h"
+
+const uint LED_PIN1 = 21;
+const uint LED_PIN2 = 22;
+const uint LED_PIN3 = 23;
+
+
+int main()
+{ 
+    stdio_init_all();
+
+    gpio_init(LED_PIN1);
+    gpio_init(LED_PIN2);
+    gpio_init(LED_PIN3);
+
+    gpio_set_dir(LED_PIN1, GPIO_OUT);
+    gpio_set_dir(LED_PIN2, GPIO_OUT);
+    gpio_set_dir(LED_PIN3, GPIO_OUT);
+
+    printf("BOOTING...\n");
+    sleep_ms(2000);
+
+    while (1) {
+
+        printf("OFF");
+        gpio_put(LED_PIN1, 0);
+        gpio_put(LED_PIN2, 0);
+        gpio_put(LED_PIN3, 0);
+
+        sleep_ms(1000);
+
+        printf("ON");
+        gpio_put(LED_PIN1, 1);
+        gpio_put(LED_PIN2, 1);
+        gpio_put(LED_PIN3, 1);
+
+        sleep_ms(1000);
+        printf("READY");
+        sleep_ms(1000);
+
+    };
+
+    return 0;
+}
