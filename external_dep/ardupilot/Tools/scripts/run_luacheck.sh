b97526d6ddd53d27eaf9c23b34cb76f228d10e1a
Ray <raymondbello11@gmail.com>
2023-04-19 21:00:49 -0400

    removed ardupilot master

--- external_dep/ardupilot/Tools/scripts/run_luacheck.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Run lua check for all lua files passing AP specific config
-
-cd "$(dirname "$0")"
-cd ../..
-
-luacheck */ --config libraries/AP_Scripting/tests/luacheck.lua
--- external_dep/ardupilot/modules/DroneCAN/libcanard/canard/helpers.h
+++
@@ -1,7 +0,0 @@
-#ifdef USE_USER_HELPERS
-#include "canard_helpers_user.h"
-#endif
-
-#ifndef NOINLINE_FUNC
-#define NOINLINE_FUNC __attribute__((noinline))
-#endif
